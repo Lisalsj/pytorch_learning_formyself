53d9aa0d90714bf98f25e67a31b908f79e7b6e75
Lisalsj <Lisalsj@outlook.com>
2024-06-21 16:00:57 +0800

    Initial commit

---
+++ read_data.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+import cv2
+import os
+
+
+class MyData(Dataset):
+
+    def __init__(self): ##初始化类，一般为整个class类增添全局变量
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)
+    int a=2
